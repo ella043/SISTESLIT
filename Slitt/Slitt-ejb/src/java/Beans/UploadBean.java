db9623658cdc5a8accefdbc05cd19841ff8c53df
Vetle Horpestad <vetle.horpestad93@gmail.com>
2015-11-30 17:11:13 +0100

    changes

--- Slitt/Slitt-ejb/src/java/Beans/UploadBean.java
+++ Slitt/Slitt-ejb/src/java/Beans/UploadBean.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package slit.ejb.filetransfer;
+//package slit.ejb.filetransfer;
 
-import javax.ejb.Stateless;
+//import javax.ejb.Stateless;
 
 
 
@@ -14,18 +14,18 @@ import javax.ejb.Stateless;
  * @author evenal
  */
 @Stateless
-public class UploadBean implements UploadBeanRemote
-{
-
-    @Override
-    public void upload(FileTransfer file) {
-        System.out.println("Uploaded file:");
-        System.out.println("Id : " + file.getId());
-        System.out.println("Filename : " + file.getFilename());
-        System.out.println("Contents : ");
-        System.out.println(file.getFileContent());
-    }
+//public class UploadBean implements UploadBeanRemote
+//{
+//
+//    @Override
+//    public void upload(FileTransfer file) {
+//        System.out.println("Uploaded file:");
+//        System.out.println("Id : " + file.getId());
+//        System.out.println("Filename : " + file.getFilename());
+//        System.out.println("Contents : ");
+//        System.out.println(file.getFileContent());
+//    }
 
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
-}
+//} 
