c03ecb320d3d8cd7b69d1f4e3924a27341dad86d
Elaine Sajets <elaine9449@live.no>
2015-12-01 01:26:12 +0100

    endret navn på korrupt fil

--- Slitt-client/src/java/phunk/Logg.java
+++ Slitt-client/src/java/phunk/Logg.java
@@ -14,6 +14,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 /**
